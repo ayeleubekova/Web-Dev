6e3c0270247d910e986c8786672bdd14f58a81bb
Aruzhan Yeleubekova <yeleubekova.aruzhan@gmail.com>
2024-02-13 19:13:18 +0600

    lab3

---
+++ lab3/Javascript info/alert.js
@@ -0,0 +1,17 @@
+alert("I'm JavaScript!");
+
+let admin, name;
+
+name = "John";
+
+admin = name;
+
+alert( admin );
+
+let name1 = "Ilya";
+
+alert( `hello ${1}` );
+
+alert( `hello ${"name"}` );
+
+alert( `hello ${name1}` );
